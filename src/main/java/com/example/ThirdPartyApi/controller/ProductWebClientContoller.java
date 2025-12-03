4849e20b05461bb3775f5f9e9a15d548c45129c8
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-12-03 15:13:10 +0530

    Using web client
    Calling third party api using web client

--- src/main/java/com/example/ThirdPartyApi/controller/ProductWebClientContoller.java
+++ src/main/java/com/example/ThirdPartyApi/controller/ProductWebClientContoller.java
@@ -1,15 +1,23 @@
 package com.example.ThirdPartyApi.controller;
 
+import com.example.ThirdPartyApi.service.ProductWebClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/webClient")
 public class ProductWebClientContoller {
 
-    @GetMapping
-    public String getProduct(){
-        return "Hello";
+    @Autowired
+    private ProductWebClientService productWebClientService;
+
+    @GetMapping("/product")
+    public Mono<List<Object>> getProduct(){
+        return productWebClientService.fetchProducts();
     }
 }
--- src/main/java/com/example/ThirdPartyApi/service/ProductWebClientService.java
+++ src/main/java/com/example/ThirdPartyApi/service/ProductWebClientService.java
@@ -1,7 +1,23 @@
 package com.example.ThirdPartyApi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 public class ProductWebClientService {
+
+    @Autowired
+    private WebClient webClient;
+
+    public Mono<List<Object>> fetchProducts(){
+        return webClient.get()
+                .uri("https://fakestoreapi.com/products/")
+                .retrieve()
+                .bodyToFlux(Object.class)
+                .collectList();
+    }
 }
